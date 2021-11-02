ac36eed93a14a20bc7943979e9487db7ca8a45ed
cmdc0de <cmdc0dez@gmail.com>
2021-11-01 20:51:02 -0700

    working

--- main.cpp
+++ main.cpp
@@ -1,18 +1,39 @@
 
 #include <cmath>
 #include <iostream>
+#include <unistd.h>
 
 
-static const int NUM_ITEMS = 24;
-static const float_t RADIUS_OF_CIRCLE = 295.0f/2.0f;
+//static const int NUM_ITEMS = 24;
+//static const float_t RADIUS_OF_CIRCLE = 295.0f/2.0f;
+static int NumItems = 0;
+static float RadiusOfCircle = 0.0f;
 static float StartingX = 0; //center pt of circle
 static float StartingY = 0;
-int main() {
+int main(int argc, char *argv[]) {
+  int c ;
+  while( ( c = getopt (argc, argv, "d:r:n:") ) != -1 ) {
+    switch(c) {
+      case 'n':
+        if(optarg) NumItems = std::atoi(optarg);
+        break;
+      case 'd':
+        if(optarg) RadiusOfCircle = std::atof(optarg)/2.0f ;
+        break;
+      case 'r':
+        if(optarg) RadiusOfCircle = std::atof(optarg);
+        break;
+    }
+  }
+  if(NumItems==0 || RadiusOfCircle<1.0f) {
+    std::cerr << "wrong params, muyst have -n and -d or -r" << std::endl;
+    return -1;
+  }
     float px,py;
-    for(int i=0;i<NUM_ITEMS;++i) {
-        px = StartingX + RADIUS_OF_CIRCLE * std::cos(2*3.14*i/NUM_ITEMS);
-        py = StartingY + RADIUS_OF_CIRCLE * std::sin(2*3.14*i/NUM_ITEMS);
-        std::cout << px << "     " << py << std::endl;
+    for(int i=0;i<NumItems;++i) {
+        px = StartingX + RadiusOfCircle * std::cos(2*3.14*i/NumItems);
+        py = StartingY + RadiusOfCircle * std::sin(2*3.14*i/NumItems);
+        std::cout << i << "      "<< px << "     " << py << std::endl;
     }
     /*
     float r = RADIUS_OF_CIRCLE;
